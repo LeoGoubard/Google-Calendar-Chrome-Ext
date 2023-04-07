0e44103d653e008c2df03a34506ad2ff46b80f07
LEO GOUBARD <goubardleo@gmail.com>
2023-04-07 18:42:27 +0200

    set

---
+++ README.md
@@ -0,0 +1,18 @@
+## Google Chrome Extension - addEvent Google Calendar
+
+Longer description explaining the rationale/intent behind the project, what it's good for, and how it works. If the next two subsections are short enough, they can be merged up into this block—perhaps as bulleted lists.
+
+### Setup Google scripts
+
+- [ ] go to https://script.google.com
+- [ ] copy paste the content of 'addEvent.txt' in a new script
+- [ ]get the "deployment ID" and paste it in the script
+- [ ] add the new extension in chrome and you are good to go!
+- [ ] ~~Decided not to do this~~
+- [x] Done!
+
+
+### Built with
+
+- Markdown
+- Love
---
+++ myscript.js
@@ -0,0 +1,28 @@
+﻿const elmTitle = document.getElementById("title");
+const elmStartTime = document.getElementById("startTime");
+const elmEndTime = document.getElementById("endTime");
+const elmAdd = document.getElementById("add");
+
+let dataTime = new Date();
+dataTime = new Date(dataTime.getTime() - dataTime.getTimezoneOffset() * 60000);
+dataTime = dataTime.toISOString();
+dataTime = dataTime.substring(0, 16);
+
+elmStartTime.value = dataTime;
+elmEndTime.value = dataTime;
+
+elmAdd.onclick = () => {
+  const title = encodeURI(elmTitle.value);
+  const startTime = elmStartTime.value;
+  const endTime = elmEndTime.value;
+
+  let url = "xxxxxxx";
+
+  url += "?title=" + title + "&startTime=" + startTime + "&endTime=" + endTime;
+  console.log(url);
+
+  fetch(url, {
+    method: "GET",
+    mode: "cors"
+  })
+}
